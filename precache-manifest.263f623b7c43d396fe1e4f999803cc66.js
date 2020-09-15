d6374e1744aac209f35ed84a7b4b50fefae75f5d
kains123 <kains0417@naver.com>
2020-09-15 15:32:12 +0900

    Updates

---
+++ precache-manifest.263f623b7c43d396fe1e4f999803cc66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e49487e11a0cc9cc79b7d729b61ef2f",
+    "url": "/Jinflix/index.html"
+  },
+  {
+    "revision": "35ddfc2b4bf440d8a403",
+    "url": "/Jinflix/static/js/2.69b29659.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Jinflix/static/js/2.69b29659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3ca5b5a7deec8d6db8",
+    "url": "/Jinflix/static/js/main.cd1549be.chunk.js"
+  },
+  {
+    "revision": "bc5a49d2dfc0a59f68d5",
+    "url": "/Jinflix/static/js/runtime-main.bee1229b.js"
+  },
+  {
+    "revision": "2acd8eb58c33535dc2190dd5a018e723",
+    "url": "/Jinflix/static/media/noPosterSmall.2acd8eb5.png"
+  }
+]);
